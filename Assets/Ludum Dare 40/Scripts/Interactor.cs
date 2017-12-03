c406e521cb361bf3a039affa166d7996e0764090
HitchH1k3r <hitch@hitchh1k3rsguide.com>
2017-12-02 19:31:13 -0500

    laptop synch (BROKEN)

---
+++ Assets/Ludum Dare 40/Scripts/Interactor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Interactor : MonoBehaviour
+{
+
+  // Configuration:
+  public float range;
+  public Sprite grabIndicator;
+
+}
+
+public interface IInteracatble
+{
+
+  public InteractionType CanInteract(Interactor actor);
+  public void Interact(InteractionType type, Interactor actor);
+
+}
+
+
